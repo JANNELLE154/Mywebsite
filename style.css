3b28505e11e22ec2e7066d2c1219c875805552ff
JANNELLE154 <jannellewilliams123@gmail.com>
2017-09-01 10:55:53 +0100

    Add files via upload

---
+++ style.css
@@ -0,0 +1,9 @@
+body {
+    background-image: url("http://www.ordup.com/images/uploads/7b83aecfac90db31b6b7034daf555825/a00ca254542b32e50bef5ba9f39e5bf5.jpg");
+}
+
+h1 {
+    color: white;
+}
+p {color: white 
+}
